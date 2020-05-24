f2b0595ae5c0cd8c1a22e8cecd166f541dac6371
Kirill Klimenko <kirill0186@gmail.com>
2020-05-24 17:41:46 +0300

    Update create student request

--- web-service/src/create_student.py
+++ web-service/src/create_student.py
@@ -7,8 +7,9 @@ from db_model import create_student
 class CreateStudent(Resource):
     def post(self):
         body = request.get_json()
+        name = body['firstName'] + ' ' + body['lastName']
 
-        student_id = create_student(body['name'])
+        student_id = create_student(name)
 
         return {
             'studentId': str(student_id)
