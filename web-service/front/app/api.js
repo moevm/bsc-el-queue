80a41cbe28783d498027d0dbf7250b2a26c0794e
Kirill Klimenko <kirill0186@gmail.com>
2020-05-24 15:51:31 +0300

    Add get room and get queue request

--- web-service/front/app/api.js
+++ web-service/front/app/api.js
@@ -2,9 +2,9 @@ import { sprintf } from 'sprintf-js'
 
 import request from '@app/lib/request'
 
-import { BASE_URL_PATH, API_VERSION, apiToken } from '@app/constants'
-
-const API_PREFIX = [BASE_URL_PATH, 'api/', `v${API_VERSION}`].join('')
+// import { BASE_URL_PATH } from '@app/constants'
+// const API_PREFIX = [BASE_URL_PATH, 'api'].join('')
+const API_PREFIX = ['http://server-test.com'].join('')
 
 // Prepare url path
 export const url = path => `${API_PREFIX}/${path}`
@@ -20,6 +20,13 @@ const apiRequest = (path, method = 'get') => ({ body, query, id }) => {
   })
 }
 
-const API = {}
+const API = {
+  room: {
+    get: apiRequest('rooms/%(roomId)s'),
+  },
+  queue: {
+    get: apiRequest('rooms/%(roomId)s/queues/%(queueId)s')
+  },
+}
 
 export default API
