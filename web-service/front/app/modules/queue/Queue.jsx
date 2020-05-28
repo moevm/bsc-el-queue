596440e502fd88484e49a1c3183a99ca8918fe04
Kirill Klimenko <kirill0186@gmail.com>
2020-05-29 01:31:07 +0300

    Update QueueStore

--- web-service/front/app/modules/queue/Queue.jsx
+++ web-service/front/app/modules/queue/Queue.jsx
@@ -1,33 +1,28 @@
 import React from 'react'
+import { observer } from 'mobx-react'
+
 import RemoteDataHOC from "@app/hocs/RemoteDataHOC";
 
+@observer
 class Queue extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      students: props.data.students
-    }
-  }
-
   componentDidMount() {
-    const { socket, queueId } = this.props
+    const { socket, queueId, fetchStore } = this.props
 
     socket.emit('students', queueId)
 
     socket.on('students', (students) => {
-      this.setState({ students })
+      fetchStore.setStudents(students)
     })
   }
 
   render() {
-    const { students } = this.state
+    const { data } = this.props
 
     return (
       <div>
         <div>Students:</div>
         <div>
-          <For each='student' of={students}>
+          <For each='student' of={data.students}>
             <div key={student.id}>{student.name}</div>
           </For>
         </div>
--- web-service/front/app/modules/queue/QueueStore.js
+++ web-service/front/app/modules/queue/QueueStore.js
@@ -1,3 +1,6 @@
+import * as R from 'ramda'
+import { action, computed } from 'mobx'
+
 import AbstractFetchStore from '@app/lib/AbstractFetchStore'
 
 import API from '@app/api'
@@ -5,5 +8,17 @@ import API from '@app/api'
 export default class QueueStore extends AbstractFetchStore {
   constructor() {
     super(API.queue.get)
+  }
+
+  @action
+  setStudents = (students) => {
+    this.data.students = students
+  }
+
+  @computed
+  get firstStudent() {
+    return this.data?.students
+    |> R.defaultTo([])
+    |> R.head
   }
 }
